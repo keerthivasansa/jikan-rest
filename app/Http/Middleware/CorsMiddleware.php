5dbb2f3250210c7c63a61a8a43fd77a7091a4fa5
Keerthi Vasan S A <sakeerthi23@gmail.com>
2023-05-22 22:26:53 +0530

    added cors to every request

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,16 +17,16 @@ class CorsMiddleware
     public function handle(Request $request, \Closure $next): Response | JsonResponse | RedirectResponse
     {
         if ($request->isMethod('OPTIONS')) {
-            $headers = [
-                'Access-Control-Allow-Origin'      => '*',
-                'Access-Control-Allow-Methods'     => 'GET, OPTIONS',
-                'Access-Control-Max-Age'           => '86400',
-                'Accept-Control-Allow-Headers' => 'Accept,Accept-Encoding,DNT,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range',
-                'Content-Type' => 'text/plain',
-                'Content-Length' => '0'
-            ];
             $response = $this->responseFactory->make("", 204, $headers);
             $response->setProtocolVersion("1.1");
+            $next($request);
+            $response->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+            ->header('Access-Control-Max-Age', '86400')
+            ->header('Accept-Control-Allow-Headers', 'Accept,Accept-Encoding,DNT,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range')
+            ->header('Content-Type', 'text/plain')
+            ->header('Content-Length', '0')
+
         }
         else {
             $response = $next($request);

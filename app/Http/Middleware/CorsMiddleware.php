6a2ac1aab54aed73d66cafa6dd3f506faf52f8fc
Keerthi Vasan S A <sakeerthi23@gmail.com>
2023-05-22 22:40:24 +0530

    fix: set content-length only for options req

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,12 +18,15 @@ class CorsMiddleware
     {
         $response = $next($request);
 
+        if ($request->isMethod('OPTIONS')) {
+            $response->header('Content-Type', 'text/plain')
+            ->header('Content-Length', '0');
+        }
+
         $response->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, OPTIONS')
         ->header('Access-Control-Max-Age', '86400')
-        ->header('Accept-Control-Allow-Headers', 'Accept,Accept-Encoding,DNT,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range')
-        ->header('Content-Type', 'text/plain')
-        ->header('Content-Length', '0');
+        ->header('Accept-Control-Allow-Headers', 'Accept,Accept-Encoding,DNT,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
 
         return $response;
     }
